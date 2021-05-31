7cf7e785a4f2fb225b3aee24b27c9a404b0fd282
Blend Rugova <73648307+blendius@users.noreply.github.com>
2021-05-31 12:18:01 +0200

    Merge

--- client-app/src/features/home/homePage.tsx
+++ client-app/src/features/home/homePage.tsx
@@ -1,24 +1,9 @@
-import React from "react";
-import { Button, Container, Form, Header } from "semantic-ui-react";
-
-export default function HomePage() {
-  return (
-    <Container style={{ width: 325, marginTop: "20em" }}>
-      <Form>
-        <Header style={{ color: "white" }} as="h1">
-          Welcome to UBT School
-        </Header>
-        <Form.Field>
-          <label style={{ color: "white" }}>Email:</label>
-          <input placeholder="Email" />
-        </Form.Field>
-        <Form.Field>
-          <label style={{ color: "white" }}>Password:</label>
-          <input placeholder="Password" />
-        </Form.Field>
-        <Form.Field></Form.Field>
-        <Button type="submit">Login</Button>
-      </Form>
-    </Container>
-  );
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+export default function HomePage(){
+    return(
+        <Container style={{marginTop:'7em'}}>
+            <h1>Home page</h1>
+        </Container>
+    )
 }
