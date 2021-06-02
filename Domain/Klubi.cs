ddb873438783ab034b7d9d06c73a305926cefdd5
Drilind <drilind@live.com>
2021-06-02 16:38:48 +0200

    klubi

---
+++ Domain/Klubi.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace Domain
+{
+    public class Klubi
+    {
+
+        public Guid Id { get; set; }
+        public string Lloji { get; set; }
+        public string Name { get; set; }
+        public string Pershkrimi { get; set; }
+        public int NrIAnetareve { get; set; }
+        public string Udheheqesi { get; set; }
+    }
+}
